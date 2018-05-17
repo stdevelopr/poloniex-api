3564b60e58321c360d6d97eb339a9d7978673093
stdevelopr <stdevelopr@gmail.com>
2018-05-17 19:26:15 -0300

    wrapper file

---
+++ wrapper.php
@@ -0,0 +1,27 @@
+<?php 
+
+$url= 'https://poloniex.com/public?command=returnTicker';
+
+//  Initiate curl
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Convert to an Json array
+$ticker = json_decode($result, true);
+
+//loop through the array and print the values
+foreach($ticker as $value=>$pair){
+	print_r($value);
+	echo '<br>';
+	print_r($pair);
+	echo '<br>';
+}
