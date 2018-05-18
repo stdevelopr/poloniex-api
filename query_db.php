bfff57fdd20d43552e60925a0cbd63e62a419c42
stdevelopr <stdevelopr@gmail.com>
2018-05-18 18:43:44 -0300

    calculating ma

--- query_db.php
+++ query_db.php
@@ -1,6 +1,12 @@
 <?php 
+use LupeCode\phpTraderNative\Trader as Trader;
+
+require_once 'vendor/autoload.php';
 
 include 'connect_db.php';
+
+$date_time = array();
+$close = array();
 
 // select all columns from database
 $sql = 'SELECT * FROM Ticker';
@@ -14,7 +20,19 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {  //the function fetch_assoc() fetch the first element from the collection.
         print_r($row);
         echo '<br>';
+        $date_time[] = $row['date_time'];
+        $close[] = $row['close'];
     }
 } else {
     echo "0 results";
 }
+
+print_r($date_time);
+echo '<br>';
+print_r($close);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$ma= Trader::ma($close, 10);
+print_r($ma);
