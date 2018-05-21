3293910b20c4e3f62fb649c5771829ff4bce6293
stdevelopr <stdevelopr@gmail.com>
2018-05-21 16:04:55 -0300

    connect to database

--- connect_db.php
+++ connect_db.php
@@ -40,18 +40,32 @@ if (mysqli_select_db($conn,'poloniex') === TRUE){
     echo "Couldn't connect to database: " . $conn->error;
     echo '<br>';
 	}
-	//Create a table containing the coins
-	$tb = 'CREATE TABLE Coins(
-	id int PRIMARY KEY AUTO_INCREMENT,
-	pair varchar(15) NOT NULL
-	)';
-
-	if ($conn->query($tb) === TRUE) {
-	    echo "Table Coins created successfully";
-	    echo '<br>';
-	} else {
-	    echo "Error creating table: " . $conn->error;
-	    echo '<br>';
-	}
 }
+
+
+// //Create a table containing the coins
+// $tb = 'CREATE TABLE Coins(
+// pair varchar(15) PRIMARY KEY
+// )';
+// if ($conn->query($tb) === TRUE) {
+//     echo "Table Coins created successfully";
+//     echo '<br>';
+// } else {
+//     echo "Error creating table: " . $conn->error;
+//     echo '<br>';
+// }
+
+
+// //Create a table containing the data
+// $tb = 'CREATE TABLE Coins(
+// pair varchar(15) PRIMARY KEY
+// )';
+// if ($conn->query($tb) === TRUE) {
+//     echo "Table Coins created successfully";
+//     echo '<br>';
+// } else {
+//     echo "Error creating table: " . $conn->error;
+//     echo '<br>';
+// }
+
 
