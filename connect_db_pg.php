eef155b2d467732efbda572693769a12ade1556b
stdevelopr <stdevelopr@gmail.com>
2018-05-24 18:37:20 -0300

    postgresql connection

---
+++ connect_db_pg.php
@@ -0,0 +1,12 @@
+<?php
+
+//Server info
+$server = 'localhost';
+$port = '5432';
+$user = 'postgres';
+$pass= '';
+$con_string = "host=localhost port=5432 dbname=poloniex user=postgres password=";
+// Create connection
+$conn = pg_connect($con_string) or die('connection failed');;
+
+echo 'Connected successfully to Postgre <BR>';
