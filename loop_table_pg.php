8b2ab24a59be01e1acfc543eeb394912ee64e1ec
stdevelopr <stdevelopr@gmail.com>
2018-05-28 18:04:38 -0300

    bootstrap added

--- loop_table_pg.php
+++ loop_table_pg.php
@@ -10,12 +10,60 @@ ob_start();
 include $vendorDir.'/poloniex-api/controller/connect_db_pg.php';
 ob_end_clean();
 
-$time_start = microtime(true);
+// $time_start = microtime(true);
 
 ?>
-<link rel="stylesheet" href="css/main.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="shortcut icon" href="/favicon.ico">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+	<!-- TableSorter -->
+	<script src="js/tablesorter.js"></script>
+	<!-- Initialize TableSorter -->
+	<script>
+		$(document).ready(function(){
+			$('#pol_table').tablesorter();
+		});
+	</script>
+</head>
+<body>
+<table class="table table-dark" id="pol_table">
+  <thead>
+    <tr>
+      <th scope="col">Pair</th>
+      <th scope="col">MACDSignal</th>
+      <th scope="col">Last Cross</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <!-- <tr>
+      <th scope="row">BTC_AMP</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr> -->
 <?
-
 // select all columns from database
 $sql = 'SELECT pair, date_time, close FROM Data';
 // runs the query and puts the resulting data into a variable
@@ -79,17 +127,24 @@ foreach ($data as $key => $value) {
 	}
 	if($i>0){
 		$status='positive';
-		$time = ($i*4).'hrs';
+		$time = ($i*4);
 	}else{
 		$status='negative';
-		$time =  ($c*4).'hrs';
+		$time =  ($c*4);
 	}
-	echo '<div class=line>';
-	echo '<p>Pair :'.$pair.'</p>';
-	echo '<p>MACD Signal: '.$status.'</p>';
-	echo '<p>Last Cross: '.$time.'</p>';
-	echo '</div>';
-	echo '<br><br>';
+	echo'
+	<tr>
+    <th scope="row">'.$pair.'</th>
+    <td>'.$status.'</td>
+    <td>'.$time.'</td>
+    <td>@mdo</td>
+    </tr>';
+	// echo '<div class=line>';
+	// echo '<p>Pair :'.$pair.'</p>';
+	// echo '<p>MACD Signal: '.$status.'</p>';
+	// echo '<p>Last Cross: '.$time.'</p>';
+	// echo '</div>';
+	// echo '<br><br>';
 
 
 
@@ -113,9 +168,15 @@ foreach ($data as $key => $value) {
 	// echo '<br>';
 };
 
-$time_end = microtime(true);
-$execution_time = ($time_end - $time_start);
-echo '<b>Total Execution Time:</b> '.$execution_time.' Mins';
+// $time_end = microtime(true);
+// $execution_time = ($time_end - $time_start);
+// echo '<b>Total Execution Time:</b> '.$execution_time.' Mins';
 
-$time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
-echo '<b>Total Execution Time:</b> '.$time;
+// $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
+// echo '<b>Total Execution Time:</b> '.$time;
+?>
+  </tbody>
+</table>
+
+</body>
+</html>
