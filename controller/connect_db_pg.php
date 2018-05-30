4b3b993003625e3b002a2f458ebc160786137fe2
stdevelopr <stdevelopr@gmail.com>
2018-05-30 17:25:31 -0300

    Connect to PostgreSQL database

--- controller/connect_db_pg.php
+++ controller/connect_db_pg.php
@@ -1,6 +1,6 @@
-<?php
-//Connect to PostgreSQL database
+<!-- Connect to PostgreSQL database -->
 
+<?php
 $con_string = "host=localhost port=5432 dbname=poloniex user=postgres password=";
 // Create connection
 $conn = pg_connect($con_string) or die('connection failed');;
