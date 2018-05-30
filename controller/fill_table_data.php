bb0e3c3f4f766758273a5d9683edd4d809f90dcd
stdevelopr <stdevelopr@gmail.com>
2018-05-30 18:04:37 -0300

    Fill MySQL Data table

--- controller/fill_table_data.php
+++ controller/fill_table_data.php
@@ -1,5 +1,5 @@
+<!-- Fill MySQL Data table -->
 <?php 
-
 ob_start();
 include 'connect_db.php';
 ob_end_clean();
@@ -19,33 +19,22 @@ $start = $end - $period*$n_candles;
 
 $sql = 'SELECT * FROM Coins';
 
-// runs the query and puts the resulting data into a variable
-$result = $conn->query($sql);  //A variable $results has a collection of rows which are returned by a query.
+$result = $conn->query($sql);
 
-//atualize the screen
 ob_implicit_flush(true);
 ob_end_flush();
 
-// initialize curl
 $ch = curl_init();
-// Disable SSL verification
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// the function num_rows() checks if there are more than zero rows returned
 if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {  //the function fetch_assoc() fetch the first element from the collection.
+    while($row = $result->fetch_assoc()) { 
     	$pair = $row['pair'];
     	$url= 'https://poloniex.com/public?command=returnChartData&currencyPair='.$pair.'&start='.$start.'&end='.$end.'&period='.$period;
-		// Set the url
 		curl_setopt($ch, CURLOPT_URL,$url);
-		// Execute
 		$get=curl_exec($ch);
-		//decode
 		$data = json_decode($get, true);
-        //loop through the array
         foreach ($data as $key => $value) {
             $date = $value['date'];
             if($date!=0){
@@ -70,7 +59,7 @@ if ($result->num_rows > 0) {
 echo 'Completed';
 echo '<br>';
 echo '<br>';
-// Closing
+
 curl_close($ch);
 
 $time_end = microtime(true);
