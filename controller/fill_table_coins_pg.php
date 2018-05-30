c3746bae25e99d583a95cc21541892343df5c9d6
stdevelopr <stdevelopr@gmail.com>
2018-05-30 18:02:57 -0300

    Fill PostgreSQL Coins table

--- controller/fill_table_coins_pg.php
+++ controller/fill_table_coins_pg.php
@@ -1,5 +1,5 @@
+<!-- Fill PostgreSQL Coins table -->
 <?php 
-
 ob_start();
 include 'connect_db_pg.php';
 ob_end_clean();
@@ -7,15 +7,12 @@ ob_end_clean();
 $url= 'https://poloniex.com/public?command=returnTicker';
 
 $ch = curl_init();
-// Disable SSL verification
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
 curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
+
 $result=curl_exec($ch);
-// Closing
+
 curl_close($ch);
 
 ob_implicit_flush(true);
