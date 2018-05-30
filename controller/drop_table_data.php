b646a6a74c43b2fc6a6017feb2d90911a04b205b
stdevelopr <stdevelopr@gmail.com>
2018-05-30 17:58:58 -0300

    Drop MySQL Data table

--- controller/drop_table_data.php
+++ controller/drop_table_data.php
@@ -1,10 +1,9 @@
+<!-- Drop MySQL Data table -->
 <?php 
-
 ob_start();
 include 'connect_db.php';
 ob_end_clean();
 
-//Create a table containing the coins
 $tb = 'DROP TABLE Data';
 if ($conn->query($tb) === TRUE) {
     echo "Table Data deleted";
